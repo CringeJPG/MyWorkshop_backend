e216e7414e371ebc1913f7911efc44f575cf6428
DoviihHawk <79831969+DoviihHawk@users.noreply.github.com>
2025-02-27 15:40:48 +0100

    Migrations done

--- database/migrations/2025_02_26_105329_create_user_follows_groups_table.php
+++ database/migrations/2025_02_26_105329_create_user_follows_groups_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('user_follows_groups', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('group_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->boolean('is_admin')->default(false);
             $table->timestamps();
         });
     }
