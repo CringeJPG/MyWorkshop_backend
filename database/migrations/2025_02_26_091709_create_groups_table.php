c83f15f1368422d9dc9f9e32009c01bb0aa64ed8
CringeJPG <lacergaming@gmail.com>
2025-03-04 09:49:36 +0100

    fix table migrations and api routes grouping

--- database/migrations/2025_02_26_091709_create_groups_table.php
+++ database/migrations/2025_02_26_091709_create_groups_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->foreignId('category_id')->constrained();
             $table->string('name');
             $table->string('description');
-            //$table->string('image'); not finished
             $table->timestamps();
         });
     }
